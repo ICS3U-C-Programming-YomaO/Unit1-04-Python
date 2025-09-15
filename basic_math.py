3a50b093d31d5b6aa713b99cbf9693d09c65db90
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-15 18:19:22 +0000

    Math values

---
+++ basic_math.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Yoma Ozoh
+# Date: September 15, 2025
+# basic math operator with my own value
+import math
+def main():
+    print(5+3)
+    print(8-4)
+    print(12*6)
+    print(7/3)
+    print(3**3)
+    print(math.sqrt(6))
+
+if __name__ == "__main__":
+    main()
