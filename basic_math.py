f70759b9ae314279d4a579b30b7abcff2b132956
Yoma Ozoh <ogeneyoma.ozoh@stu.ocsb.ca>
2025-09-17 18:16:32 +0000

    Python basic math

--- basic_math.py
+++ basic_math.py
@@ -2,14 +2,17 @@
 # Created By: Yoma Ozoh
 # Date: September 15, 2025
 # basic math operator with my own value
-import math
+
+
 def main():
-    print(5+3)
-    print(8-4)
-    print(12*6)
-    print(7/3)
-    print(3**3)
-    print(math.sqrt(6))
+    print("5+3 = {}".format(5 + 3))
+    print("8-3 = {}".format(8 - 4))
+    print("12*6 = {}".format(12 * 6))
+    print("7/2 = {}".format(7 / 3))
+    print("2^3 = {}".format(2 ^ 3))  # bitwise operator
+    print("3**3 = {}".format(3**3))  # exponent
+    print("81**0.5 = {}".format(81**0.5))  # square root
+
 
 if __name__ == "__main__":
     main()
